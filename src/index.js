8590138a96d180574b0c35d425a262b427656122
Rizwan Atta <rizwanatta890@gmail.com>
2019-11-29 11:19:35 +0500

    splash part two on way

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/*
+| this will contain all the exports going to and fro from src
+| this lessens the architectural imports of ../../
+*/
+
+export {default} from './scenes/Splash'
---
+++ src/theme/theme.js
@@ -0,0 +1,11 @@
+/*
+| this contains the basic theme to be used using context all over
+| the RN-Element based component along the custom stylesheets
+*/
+const theme = {
+  colors: {
+    primary: 'purple',
+  },
+};
+
+export default theme;
